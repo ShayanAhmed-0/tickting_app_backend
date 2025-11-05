a6cbd26a4b4eb6160042ec836721d4cc9c336663
ShayanAhmed-0 <shayanlost1@gmail.com>
2025-11-05 05:29:31 +0500

    Refactor currency update logic in admin controller and validation schema to use uppercase property names (USD, MXN) for consistency and improved clarity in API requests.

--- src/validators/adminValidators/misc.validator.ts
+++ src/validators/adminValidators/misc.validator.ts
@@ -1,9 +1,9 @@
 import { z, ZodSchema } from "zod";
 
 export const updateCurrencySchema: ZodSchema<{
-  usd: number;
-  mxn: number;
+  USD: number;
+  MXN: number;
 }> = z.object({
-  usd: z.number(),
-  mxn: z.number(),
+  USD: z.number(),
+  MXN: z.number(),
 });
